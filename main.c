2230db4de3a381efd9b8096cffa5e4b62785ec73
Romano Schoonheim <stal@romanoschoonheim.nl>
2022-07-04 08:18:46 +0200

    Read file line by line.

--- main.c
+++ main.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+
+    /**
+     * Start by opening a file.
+     */
+    FILE *stream;
+    stream = fopen("/home/romano/CLionProjects/lexical-analysis/Files/empty.language", "r");
+
+    if (stream == NULL) {
+        printf("Error while opening the given file.");
+        return 1;
+    }
+
+    /**
+     * Reading the file line by line.
+     */
+
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+
+    while ((read = getline(&line, &len, stream)) != -1) {
+        printf("Line length %zu:\n", read);
+        printf("%s", line);
+    }
+
+
     return 0;
 }
